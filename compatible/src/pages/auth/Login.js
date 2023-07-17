04f1a44f13151e3806b937abafdacc161f42d21a
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-17 16:34:18 -0400

    Update login auth route to remove redirect

--- compatible/src/pages/auth/Login.js
+++ compatible/src/pages/auth/Login.js
@@ -1,67 +1,76 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const onSubmit = (e) => {
+  useEffect(() => {
+    if (localStorage.getItem('token') !== null) {
+      window.location.replace('http://localhost:3000/');
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  const onSubmit = e => {
     e.preventDefault();
 
     const user = {
       email: email,
-      password: password,
+      password: password
     };
-
-    setLoading(true);
 
     fetch('http://127.0.0.1:8000/api/v1/users/auth/login/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(user)
     })
-      .then((res) => res.json())
-      .then((data) => {
-        setLoading(false);
+      .then(res => res.json())
+      .then(data => {
         if (data.key) {
+          localStorage.clear();
           localStorage.setItem('token', data.key);
           window.location.replace('http://localhost:3000/');
         } else {
           setEmail('');
           setPassword('');
-          setError('Invalid email or password');
+          localStorage.clear();
+          setErrors(true);
         }
-      })
-      .catch((error) => {
-        setLoading(false);
-        setError('An error occurred. Please try again later.');
       });
   };
 
   return (
     <div>
-      <h2>Login</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? 'Loading...' : 'Login'}
-        </button>
-      </form>
+      {loading === false && <h1>Login</h1>}
+      {errors === true && <h2>Cannot log in with provided credentials</h2>}
+      {loading === false && (
+        <form onSubmit={onSubmit}>
+          <label htmlFor='email'>Email address:</label> <br />
+          <input
+            name='email'
+            type='email'
+            value={email}
+            required
+            onChange={e => setEmail(e.target.value)}
+          />{' '}
+          <br />
+          <label htmlFor='password'>Password:</label> <br />
+          <input
+            name='password'
+            type='password'
+            value={password}
+            required
+            onChange={e => setPassword(e.target.value)}
+          />{' '}
+          <br />
+          <input type='submit' value='Login' />
+        </form>
+      )}
     </div>
   );
 };
