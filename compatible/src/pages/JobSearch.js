268d052b5f6233eac622e1acb9d574fac4ca679e
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-16 21:26:02 -0400

    Add error handling for job search api funcationality

--- compatible/src/pages/JobSearch.js
+++ compatible/src/pages/JobSearch.js
@@ -3,8 +3,10 @@ import { Col, Row } from 'antd';
 
 function JobSearch() {
     const [jobs, setJobs] = useState([]);
+    const [errorMessages, setErrorMessages] = useState({})
     const [keyword, setKeyword] = useState("");
     const [location, setLocation] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const url = `https://indeed-jobs-api.p.rapidapi.com/indeed-us/?offset=0&keyword=${keyword}&location=${location}`;
     const options = {
@@ -15,25 +17,57 @@ function JobSearch() {
         }
     };
 
-    async function fetchJobs(e) {
-        e.preventDefault()
-        try {
-            const response = await fetch(url, options);
-            console.log(response);
-            console.log(response.body);
-            const result = await response.json();
-            console.log(result);
+    const validateSelect = async () => {
+        setJobs([]);
+        setErrorMessages({});
 
-            setJobs(result);
-            console.log(setJobs);
+        try {
+            setIsLoading(true);
+            const response = await fetch(url, options);
+            const result = await response.json()
+
+            if (result.Error) {
+                setErrorMessages({
+                    ...errorMessages, location: 'Please make a valid selection that includes a keyword and city or state in the USA'
+                });
+                setIsLoading(false);
+                return false;
+            } else {
+                setErrorMessages({});
+                setJobs(result);
+                setIsLoading(false);
+                return true;
+            };
         } catch (err) {
             console.log(err);
+            setIsLoading(false);
+            return false;
+        }
+    };
+
+    const fetchJobs = async (e) => {
+        e.preventDefault();
+        if (await validateSelect()) {
+            try {
+                setIsLoading(true);
+                setJobs([]);
+                setErrorMessages({});
+                const response = await fetch(url, options);
+                const result = await response.json();
+                setJobs(result);
+                setIsLoading(false);
+            } catch (err) {
+                console.log(err);
+                setIsLoading(false);
+            }
         }
     };
 
     const handleChange = (e) => {
         setKeyword('');
         setLocation('');
+        setJobs([]);
+        setErrorMessages({});
     }
 
     return (
@@ -57,20 +91,22 @@ function JobSearch() {
                 </Row>
                 <hr />
                 <Row gutter={[25, 25]} justify="left" align="top">
-                    {
-                        jobs && !jobs.Error && jobs !== [] && jobs.map((job, idx) => {
-                            return (
-                                <Col span={6} key={idx}>
-                                    <div className="charging-card">
-                                        <h3>Job title: {job.job_title}</h3>
-                                        <h6>Company: {job.company_name}</h6>
-                                        <h6>Location: {job.job_location}</h6>
-                                        {/* <h6>More info: {job.job_url}</h6> */}
-                                    </div>
-                                </Col>
-                            )
-                        })
-                    }
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : jobs && jobs.length > 0 ? (
+                        jobs.map((job, idx) => (
+                            <Col span={6} key={idx}>
+                                <div className="charging-card">
+                                    <h3>Job title: {job.job_title}</h3>
+                                    <h6>Company: {job.company_name}</h6>
+                                    <h6>Location: {job.job_location}</h6>
+                                    {/* <h6>More info: {job.job_url}</h6> */}
+                                </div>
+                            </Col>
+                        ))
+                    ) : (
+                        <p>{errorMessages.location ? errorMessages.location : null}</p>
+                    )}
                 </Row>
             </div>
         </>
