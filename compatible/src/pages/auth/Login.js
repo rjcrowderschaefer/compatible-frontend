21ee025d0eb9d7b4fb6e521a27582f871b2cc896
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-17 13:29:16 -0400

    Build use auth functionality - login, logout, signup

---
+++ compatible/src/pages/auth/Login.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    setLoading(true);
+
+    fetch('http://127.0.0.1:8000/api/v1/users/auth/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        if (data.key) {
+          localStorage.setItem('token', data.key);
+          window.location.replace('http://localhost:3000/dashboard');
+        } else {
+          setEmail('');
+          setPassword('');
+          setError('Invalid email or password');
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError('An error occurred. Please try again later.');
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? 'Loading...' : 'Login'}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
