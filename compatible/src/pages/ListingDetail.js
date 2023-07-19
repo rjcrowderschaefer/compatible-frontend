dd48c329bb4f5cc307a15e9507af12ef23d24fec
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-19 16:35:22 -0400

    Add styling to listing detail page

--- compatible/src/pages/ListingDetail.js
+++ compatible/src/pages/ListingDetail.js
@@ -38,31 +38,31 @@ function ListingDetails(props) {
     function loaded() {
         return (
             <>
-                <div key={id}>
-                    <h2>{listing.listing_name}</h2>
-                    <h3>{listing.listing_description}</h3>
-                    <img src={listing.listing_img1}></img>
-                    <img src={listing.listing_img2}></img>
-                    <img src={listing.listing_img3}></img>
-                    <h4>{listing.listing_type}</h4>
-                    <h4>{listing.listing_comp_type}</h4>
-                    <h4>{listing.listing_status}</h4>
-                    <h4>{listing.created_at}</h4>
-                    <h4>Listing category: <Categories /></h4>
+                <div className="category-details-card" key={id}>
+                    <h1 className="categories-header">{listing.listing_name}</h1>
+                    <img className="category-detail-img" src={listing.listing_img1}></img>
+                    <h3 className="category-detail-desc">{listing.listing_description}</h3>
+                    <img className="category-detail-img" src={listing.listing_img2}></img>
+                    <img className="category-detail-img" src={listing.listing_img3}></img>
+                    <h4 className="listing-detail detail-page">Request type: {listing.listing_type}</h4>
+                    <h4 className="listing-detail detail-page">Compensation: {listing.listing_comp_type}</h4>
+                    <h4 className="listing-detail detail-page">Status: {listing.listing_status}</h4>
+                    <h4 className="listing-detail detail-page">Created: {listing.created_at}</h4>
+                    <h4 className="listing-detail detail-page">Category: <Categories /></h4>
 
-                    <div className="modify-trip">
+                    <div className="modify-listing">
                         <Link to={`/listings/${id}/edit`}>
-                            <button>Edit</button>
+                            <button className="edit-listing">Edit</button>
                         </Link>
                         <Link to={`/listings/${id}/delete`}>
-                            <button>Delete</button>
+                            <button className="delete-listing">Delete</button>
                         </Link>
                     </div>
 
-                    <hr />
+                    {/* <hr />
                     List of related listings by category (carousel linked to?)<br />
                     Add links to edit, delete listing
-                    Add link to create new listing (sub nav that only displays when on browse listings, listing detail pages?)
+                    Add link to create new listing (sub nav that only displays when on browse listings, listing detail pages?) */}
                 </div>
             </>
         )
