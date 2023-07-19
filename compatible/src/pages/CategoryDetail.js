cb0aa4d74730356340775d0230fedf01a8c980ed
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-19 16:05:33 -0400

    Add styling to category detail page

--- compatible/src/pages/CategoryDetail.js
+++ compatible/src/pages/CategoryDetail.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { categoriesDetailLoader } from '../apiCalls';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleDot, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 function CategoryDetails() {
 
@@ -23,16 +25,15 @@ function CategoryDetails() {
     function loaded() {
         return (
             <>
-                <div key={id}>
-                    <h2>{category.category_name}</h2>
-                    <h3>{category.category_description}</h3>
-                    <img src={category.category_img1}></img>
-                    <img src={category.category_img2}></img>
-                    <img src={category.category_img3}></img>
-                    <h4>Active listings: {category.active_listing_total}</h4>
-                    <hr />
-                    List of active listings (linked to)<br />
-                    Add new listing link to listing create form
+                <div className="category-details-card" key={id}>
+                    <h1 className="categories-header">{category.category_name}</h1>
+                    <img className="category-detail-img" src={category.category_img1} />
+                    <h3 className="category-detail-desc">{category.category_description}</h3>
+                    <img className="category-detail-img" src={category.category_img2}></img>
+                    <img className="category-detail-img" src={category.category_img3}></img>
+                    {/* <h4>Active listings: {category.active_listing_total}</h4> */}
+                    {/* List of active listings (linked to)<br />
+                    Add new listing link to listing create form */}
                 </div>
             </>
         )
